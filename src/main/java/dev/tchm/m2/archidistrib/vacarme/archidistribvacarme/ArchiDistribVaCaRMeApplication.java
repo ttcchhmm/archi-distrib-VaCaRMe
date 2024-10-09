d82740332d9d11b5af39f59253488128d21deaf8
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2024-10-09 11:32:25 +0200

    Initial commit

---
+++ src/main/java/dev/tchm/m2/archidistrib/vacarme/archidistribvacarme/ArchiDistribVaCaRMeApplication.java
@@ -0,0 +1,13 @@
+package dev.tchm.m2.archidistrib.vacarme.archidistribvacarme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiDistribVaCaRMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiDistribVaCaRMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/tchm/m2/archidistrib/vacarme/archidistribvacarme/ArchiDistribVaCaRMeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tchm.m2.archidistrib.vacarme.archidistribvacarme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiDistribVaCaRMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
